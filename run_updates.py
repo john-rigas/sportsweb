bbfcdc63e500790a7459ad245b93f2e12ee6901f
john-rigas <jmr294@georgetown.edu>
2019-09-10 07:25:01 +0100

    added cron script

---
+++ run_updates.py
@@ -0,0 +1,21 @@
+import os
+import django
+import argparse
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sportsweb.settings")
+django.setup()
+
+from nfl import models
+
+job_dict = {
+    'r': models.load_results_to_db_from_pl,
+    'p': models.update_player_records,
+    't': models.update_team_records
+}
+
+parser = argparse.ArgumentParser(description='run cronjobs')
+parser.add_argument('jobs', default = job_dict.keys())
+args = parser.parse_args()
+
+for key in args:
+    job_dict[key]()
