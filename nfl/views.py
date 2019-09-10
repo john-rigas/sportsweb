193fbc564f15951b5b31f64ef15a0cd3c0685ba4
john-rigas <jmr294@georgetown.edu>
2019-09-10 04:13:42 +0100

    fixed condition for adding family again

--- nfl/views.py
+++ nfl/views.py
@@ -15,7 +15,7 @@ def home_page(request):
     if not Team.objects.all():
         load_teams_to_db() #should elim from regular website load
         load_schedule_to_db_from_pl() # same as above
-    if not Player.objects.get(name = 'johnny'):
+    if not Player.objects.all():
         create_family_players()
     if os.path.exists('manual_picks.pl'):
         update_selections_from_pl()
