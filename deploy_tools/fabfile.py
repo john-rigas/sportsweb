419a1efa20e7ae6846dd96368511338cc50d8aa9
john-rigas <jmr294@georgetown.edu>
2019-09-12 18:48:00 +0100

    quick test 4

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -47,8 +47,8 @@ def _update_database():
 def _add_cronjobs():
     run('crontab -r')
     print ('test 0')
-    run('crontab -l')
-    run('crontab -l > /tmp/crondump')             
+    #run('crontab -l')
+    #run('crontab -l > /tmp/crondump')             
     run('echo "*/15 * * * * export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:~/sites/fredandfred.tk/virtualenv/lib/python3.6/site-packages/selenium/webdriver/firefox/webdriver.py && DISPLAY=:0 && cd ~/sites/fredandfred.tk  &&  ./virtualenv/bin/python save_schedule.py" >> /tmp/crondump')
     print ('test 1')
     run('crontab -l')
