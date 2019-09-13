a089ab34ab49a1af6f419572abbe43df4cb8e214
john-rigas <jmr294@georgetown.edu>
2019-09-13 02:58:13 +0100

    go

---
+++ send_backup_email.py
@@ -0,0 +1,18 @@
+import pickle
+from django.core.mail import send_mail
+import os
+import django
+from sportsweb import settings
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sportsweb.settings")
+django.setup()
+
+with open('email_backup.pl','rb') as f:
+    message = pickle.load(f)
+send_mail('Picks backup',
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [settings.DEFAULT_FROM_EMAIL],
+            fail_silently=False)   
+
