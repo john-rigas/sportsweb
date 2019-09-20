544645e25a8efa22e0a60e7f68e1ee3cbfae0734
john-rigas <jmr294@georgetown.edu>
2019-09-20 20:17:34 +0100

    emailing db too 4

--- send_backup_email.py
+++ send_backup_email.py
@@ -14,11 +14,10 @@ with open('email_backup.pl','rb') as f:
 message = EmailMessage('Picks backup',
                        message_text, 
                        settings.DEFAULT_FROM_EMAIL,
-                       [settings.DEFAULT_FROM_EMAIL],
-                       fail_silently=False)
+                       [settings.DEFAULT_FROM_EMAIL])
 
 message.attach_file('db.sqlite3')
-message.send()
+message.send(fail_silently=False)
 
 os.remove('email_backup.pl')
 
