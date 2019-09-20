9ecd73e5f04320d70dbc96d624131c3d46a1704c
john-rigas <jmr294@georgetown.edu>
2019-09-20 20:04:50 +0100

    emailing db too

--- send_backup_email.py
+++ send_backup_email.py
@@ -1,5 +1,5 @@
 import pickle
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 import os
 import django
 from sportsweb import settings
@@ -8,13 +8,16 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sportsweb.settings")
 django.setup()
 
 with open('email_backup.pl','rb') as f:
-    message = pickle.load(f)
+    message_text = pickle.load(f)
 
-send_mail('Picks backup',
-            message,
-            settings.DEFAULT_FROM_EMAIL,
-            [settings.DEFAULT_FROM_EMAIL],
-            fail_silently=False)
+message = EmailMessage('Picks backup',
+                       message_text, 
+                       settings.DEFAULT_FROM_EMAIL,
+                       [settings.DEFAULT_FROM_EMAIL],
+                       fail_silently=False)
+
+message.attach_file('db.sqlite3')
+message.send()
 
 os.remove('email_backup.pl')
 
